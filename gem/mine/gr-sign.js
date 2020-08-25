3cc5327eb22d544b4e106f8ad6725a2b0e555336
Carlomagno Lopez <clopez@mxitgo.com>
2020-08-25 17:13:50 -0500

    add changes for new service

--- gem/mine/gr-sign.js
+++ gem/mine/gr-sign.js
@@ -449,7 +449,8 @@ $('.two > section > div > div form > .submit.global').on('click', function(e) {
                 style: {  position: 'fixed', width: '100%', height: '100%', background: 'rgba(0, 0, 0, .8)', left: 0, top: 0, zIndex: 10000 }
             });
             var username  = $("#txtUsername").val();
-            $("#txtEmail").val(username+'@phpmedical.com');
+            // $("#txtEmail").val(username+'@phpmedical.com');
+            $("#txtEmail").val(username);
             var phone     = $("#selComplementPhone").val() + $("#txtPhoneNumber").val();
             var exist     = existUser(phone,username);
             
